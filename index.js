ad28d9ed2e1c2fafd17e16b0b1fde1ab0b39ef08
E Berry <elliot@daddy.cat>
2023-08-08 12:56:33 -0400

    dunno

--- index.js
+++ index.js
@@ -1,2 +1,134 @@
-import mic from "./lib/mic.js";
-export default mic;
+import {spawn} from 'child_process';
+import * as os from 'os';
+const isMac = os.type() == 'Darwin';
+const isWindows = os.type().includes('Windows');
+import IsSilence from './lib/silenceTransform.js';
+import {PassThrough as PassThrough} from 'stream';
+
+class Mike {
+  constructor(options) {
+    let optionDefaults = {
+      rate: '16000',
+      channels: '1',
+      debug: false,
+      exitOnSilence: 6,
+      fileType: 'raw',
+      endian: 'little',
+      bitwidth: '16',
+      encoding: 'signed-integer',
+      device: 'plughw:1,0',
+    };
+
+    let actualOptions = Object.assign({}, optionDefaults, options);
+    const {rate, channels, debug, exitOnSilence, fileType, endian, bitwidth, encoding, device} = actualOptions;
+
+    this.format = null;
+    this.formatEndian = null;
+    this.formatEncoding = null;
+    this.audioProcess = null;
+    this.infoStream = new PassThrough();
+    this.audioStream = new IsSilence({debug});
+    this.rate = rate;
+    this.channels = channels;
+    this.debug = true;
+    this.device = device;
+    this.fileType = fileType;
+    this.exitOnSilence = exitOnSilence;
+    this.endian = endian;
+    this.bitwidth = bitwidth;
+    this.encoding = encoding;
+
+    if (debug) {
+      this.infoStream.on('data', data => {
+        console.log(`Received Info: ${data}`);
+      });
+      this.infoStream.on('error', error => {
+        console.log(`Error in Info Stream: ${error}`);
+      });
+    }
+    const audioProcessOptions = {
+      stdio: ['ignore', 'pipe', 'ignore'],
+    };
+
+    if (debug) {
+      audioProcessOptions.stdio[2] = 'pipe';
+    }
+
+    // Setup format variable for arecord call
+    if (endian === 'big') {
+      this.formatEndian = 'BE';
+    } else {
+      this.formatEndian = 'LE';
+    }
+    if (encoding === 'unsigned-integer') {
+      this.formatEncoding = 'U';
+    } else {
+      this.formatEncoding = 'S';
+    }
+    this.format = `${this.formatEncoding + bitwidth}_${this.formatEndian}`;
+    this.audioStream.setNumSilenceFramesExitThresh(parseInt(exitOnSilence, 10));
+    this.start();
+    return this;
+  }
+
+  start() {
+    if (this.audioProcess === null) {
+      if (isWindows) {
+        this.audioProcess = spawn('sox', ['-b', this.bitwidth, '--endian', this.endian, '-c', this.channels, '-r', this.rate, '-e', this.encoding, '-t', 'waveaudio', 'default', '-p'], this.audioProcessOptions);
+      } else if (isMac) {
+        this.audioProcess = spawn('rec', ['-b', this.bitwidth, '--endian', this.endian, '-c', this.channels, '-r', this.rate, '-e', this.encoding, '-t', this.fileType, '-'], this.audioProcessOptions);
+      } else {
+        this.audioProcess = spawn('arecord', ['-t', this.fileType, '-c', this.channels, '-r', this.rate, '-f', this.format, '-D', this.device], this.audioProcessOptions);
+      }
+
+      this.audioProcess.on('exit', (code, sig) => {
+        if (code != null && sig === null) {
+          this.audioStream.emit('audioProcessExitComplete');
+          if (this.debug) console.log('recording audioProcess has exited with code = %d', code);
+        }
+      });
+      this.audioProcess.stdout.pipe(this.audioStream);
+      if (this.debug) {
+        this.audioProcess.stderr.pipe(this.infoStream);
+      }
+      this.audioStream.emit('startComplete');
+    } else {
+      if (this.debug) {
+        throw new Error('Duplicate calls to start(): Microphone already started!');
+      }
+    }
+  }
+
+  stop() {
+    if (this.audioProcess != null) {
+      this.audioProcess.kill('SIGTERM');
+      this.audioProcess = null;
+      this.audioStream.emit('stopComplete');
+      if (this.debug) console.log('Microphone stopped');
+    }
+  }
+
+  pause() {
+    if (this.audioProcess != null) {
+      this.audioProcess.kill('SIGSTOP');
+      this.audioStream.pause();
+      this.audioStream.emit('pauseComplete');
+      if (this.debug) console.log('Microphone paused');
+    }
+  }
+
+  resume() {
+    if (this.audioProcess != null) {
+      this.audioProcess.kill('SIGCONT');
+      this.audioStream.resume();
+      this.audioStream.emit('resumeComplete');
+      if (this.debug) console.log('Microphone resumed');
+    }
+  }
+
+  getAudioStream() {
+    return this.audioStream;
+  }
+}
+
+export default Mike;
--- tests/test.js
+++ tests/test.js
@@ -1,55 +1,53 @@
-import mic from "../index.js";
-import fs from "fs";
+import mike from '../index.js';
+import fs from 'fs';
 
-var micInstance = mic({ 'rate': '16000', 'channels': '1', 'debug': false, 'exitOnSilence': 6 });
-var micInputStream = micInstance.getAudioStream();
+const micInstance = new mike({rate: '16000', channels: '1', debug: false, exitOnSilence: 6});
+const micInputStream = micInstance.getAudioStream();
 
-var outputFileStream = fs.WriteStream('output.raw');
+const outputFileStream = fs.WriteStream('output.raw');
 
 micInputStream.pipe(outputFileStream);
 
-var chunkCounter = 0;
-micInputStream.on('data', function(data) {
-        console.log("Recieved Input Stream of Size %d: %d", data.length, chunkCounter++);
+let chunkCounter = 0;
+micInputStream.on('data', ({length}) => {
+  console.log('Recieved Input Stream of Size %d: %d', length, chunkCounter++);
 });
 
-micInputStream.on('error', function(err) {
-    cosole.log("Error in Input Stream: " + err);
+micInputStream.on('error', err => {
+  console.log(`Error in Input Stream: ${err}`);
 });
 
-micInputStream.on('startComplete', function() {
-        console.log("Got SIGNAL startComplete");
-        setTimeout(function() {
-                micInstance.pause();
-            }, 5000);
-    });
-    
-micInputStream.on('stopComplete', function() {
-        console.log("Got SIGNAL stopComplete");
-    });
-    
-micInputStream.on('pauseComplete', function() {
-        console.log("Got SIGNAL pauseComplete");
-        setTimeout(function() {
-                micInstance.resume();
-            }, 5000);
-    });
+micInputStream.on('startComplete', () => {
+  console.log('Got SIGNAL startComplete');
+  setTimeout(() => {
+    micInstance.pause();
+  }, 5000);
+});
 
-micInputStream.on('resumeComplete', function() {
-        console.log("Got SIGNAL resumeComplete");
-        setTimeout(function() {
-                micInstance.stop();
-            }, 5000);
-    });
+micInputStream.on('stopComplete', () => {
+  console.log('Got SIGNAL stopComplete');
+});
 
-micInputStream.on('silence', function() {
-        console.log("Got SIGNAL silence");
-    });
+micInputStream.on('pauseComplete', () => {
+  console.log('Got SIGNAL pauseComplete');
+  setTimeout(() => {
+    micInstance.resume();
+  }, 5000);
+});
 
-micInputStream.on('processExitComplete', function() {
-        console.log("Got SIGNAL processExitComplete");
-    });
+micInputStream.on('resumeComplete', () => {
+  console.log('Got SIGNAL resumeComplete');
+  setTimeout(() => {
+    micInstance.stop();
+  }, 5000);
+});
+
+micInputStream.on('silence', () => {
+  console.log('Got SIGNAL silence');
+});
+
+micInputStream.on('processExitComplete', () => {
+  console.log('Got SIGNAL processExitComplete');
+});
 
 micInstance.start();
-
-
